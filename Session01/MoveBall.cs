75dbed7b0aa8d89e209091a3eb97b02c1d3a780e
cdburner <wrightdg@lsbu.ac.uk>
2016-07-01 15:36:16 +0100

    Added header comments

--- Session01/MoveBall.cs
+++ Session01/MoveBall.cs
@@ -1,4 +1,24 @@
-﻿using UnityEngine;
+﻿/*
+
+ * transform position refers to the current position of the 
+ * gameObject that this script is attached to
+ * 
+ * set:
+ * To change the transform.position you need to give it
+ * a Vector2/3 object
+ * 
+ * get:
+ * transform.position returns a Vector3 object of the current
+ * position of the gameObject
+ * You can also get singly the x, y or z value 
+ * For example to get the x position we would write
+ * transform.position.x
+ * 
+ * 
+
+*/
+
+using UnityEngine;
 using System.Collections;
 
 public class MoveBall : MonoBehaviour {
