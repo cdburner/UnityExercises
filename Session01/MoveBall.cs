c3ed742d6307f6d88c109bfead57fb64ecc41a3e
David Wright <bbk@1-2-1-training.co.uk>
2016-07-01 11:24:37 +0100

    First commit
    • Created simple code that moves an gameObject

---
+++ Session01/MoveBall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (transform.position);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2(transform.position.x + 0.05f, 0);
+	}
+}
